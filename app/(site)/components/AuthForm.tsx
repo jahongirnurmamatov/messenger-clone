6c5dd79ee26faa759e2458cf973f4350e2644cca
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-08-19 14:39:11 +0500

    login with credentials

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -8,6 +8,8 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import AuthSocialButton from "./AuthSocialButton";
 import { BsGithub, BsGoogle } from "react-icons/bs";
 import axios from "axios";
+import toast from "react-hot-toast";
+import { signIn } from "next-auth/react";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -38,10 +40,24 @@ const AuthForm = () => {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsloading(true);
     if (variant === "REGISTER") {
-      axios.post('/api/register',data);
+      axios.post('/api/register',data)
+      .catch(()=>toast.error('Something went wrong'))
+      .finally(()=>setIsloading(false))
     }
     if (variant === "LOGIN") {
-      
+      signIn('credentials',{
+        ...data,
+        redirect:false
+      })
+      .then((callback)=>{
+        if(callback?.error){
+          toast.error('Invalid credentials');
+        }
+        if(callback?.ok && !callback?.error){
+          toast.success('Logged in');
+        }
+      })
+      .finally(()=>setIsloading(false));
     }
   };
   const socialAction = (action: string) => {
